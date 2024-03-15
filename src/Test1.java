7f5930a5bfe3d8ae829d37299055a5bde00b70b0
s <c>
2024-03-15 17:15:38 +0530

    removed todo

--- src/Test1.java
+++ src/Test1.java
@@ -1,7 +1,7 @@
 public class Test1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
       String drive="yes";
 		if(drive=="yes")
 		System.out.println("go for drive");
