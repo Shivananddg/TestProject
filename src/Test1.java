c0845f76c34cfc2f4aec30a2f9426f78f6e1a3e9
s <c>
2024-03-15 15:53:44 +0530

    first commit

---
+++ src/Test1.java
@@ -0,0 +1,13 @@
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String drive="yes";
+		if(drive=="yes")
+		System.out.println("go for a long drive");
+		else {
+			System.out.println("learn driving");	
+		}
+	}
+
+}
